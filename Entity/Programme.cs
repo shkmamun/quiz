30b4033e3b13f3e4dca0fc6dc3a7236f243d1a93
shkmamun <shkmamun@gmail.com>
2015-01-19 18:49:19 +0600

    added entity

---
+++ Entity/Programme.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Entity
+{
+    public class Programme
+    {       
+        public int ProgrammeId { set; get; }
+
+        [Required]      
+        [Display(Name = "Programme Name")]
+        public string ProgrammeName { set; get; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name="Start Date")]
+        public DateTime StartDate { set; get; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { set; get; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Num of Question")]
+        public int NumOfQuestion { set; get; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Time Limit")]
+        public int TimeLimit { set; get; }
+
+        [Required]
+        [Display(Name = "Is Hourly")]
+        [DefaultValue(true)]
+        public bool IsHourly { set; get; }
+
+        [Required]
+        [Display(Name = "Is Active")]
+        [DefaultValue(true)]
+        public bool IsActive { set; get; }
+    }
+}
